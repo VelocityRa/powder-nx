fd16ae2fd367662af85b32f537f70d9eca94361e
Nick Renieris <velocityra@gmail.com>
2019-09-07 21:06:21 +0300

    build/switch: Build/deploy scripts

---
+++ build.sh
@@ -0,0 +1,1 @@
+scons --switch -j5 $@
---
+++ nro.sh
@@ -0,0 +1,3 @@
+pushd build
+/opt/devkitpro/tools/bin/elf2nro powder-nx powder-nx.nro --icon="$PWD/../resources/icon/powder-256.png" --nacp="$PWD/../resources/control.nacp"
+popd
